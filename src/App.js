c856cd3d79fa703ab623bcbcb8a754c538e65eea
Bryan Emerson <bryan.l.emerson@gmail.com>
2021-03-07 15:06:57 -0500

    trying to set up receipt component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let receipts = [
+  {
+    person: "Karolin",
+    order: {
+      main: "Burrito",
+      protein: "Organic Tofu",
+      rice: "Purple Rice",
+      sauce: "Green Crack",
+      toppings: ["Baby Bok Choy", "Cucumber Kimchi"],
+      drink: "Korchata",
+      cost: 22
+    },
+    paid: false
+  },
+  {
+    person: "Jerrica",
+    order: {
+      main: "Rice Bowl",
+      protein: "Ginger Soy Chix",
+      rice: "Sticky Rice",
+      sauce: "Korilla",
+      toppings: ["Yuzu Pickled Sweet Pepper", "Kale"],
+      drink: "Korchata",
+      cost: 19
+    },
+    paid: false
+  },
+  {
+    person: "Matt",
+    order: {
+      main: "Salad Bowl",
+      protein: "Organic Tofu",
+      rice: "none",
+      sauce: "K'lla",
+      toppings: ["Blue Potato Salad", "Pico De Gallo", "Red Kimchi"],
+      drink: "Sparkling Blood Orange Soda",
+      cost: 20
+    },
+    paid: false
+  }
+]
+console.log(receipts[0])
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = receipts[0];
+  }
 
   render () {
     return (
---
+++ src/Receipt.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './App';
+
+class Receipt extends Component {
+  constructor(props) {
+    super(props)
+  }
+}
+
+export default Receipt;
